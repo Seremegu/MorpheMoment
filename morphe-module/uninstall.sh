j-hc/revanced-magisk-module

module: fix issues occuring from stock system app being installed

module/uninstall.sh
@@ -1,13 +1,9 @@
 #!/system/bin/sh
-{
 
-	until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
-	until [ -d "/sdcard/Android" ]; do sleep 1; done
+MODDIR=${0%/*}
+. "$MODDIR/config"
 
-	MODDIR=${0%/*}
-	. "$MODDIR/config"
+rm -f "/data/adb/rvhc/${MODDIR##*/}.apk"
+rmdir "/data/adb/rvhc"
 
-	rm "/data/adb/rvhc/${MODDIR##*/}.apk"
-	rmdir "/data/adb/rvhc"
-	rm "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
-} &
+rm -f "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"

Seremegu/MorpheMoment

Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

morphe-module/uninstall.sh
@@ -1,13 +1,9 @@
 #!/system/bin/sh
-{
 
-	until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
-	until [ -d "/sdcard/Android" ]; do sleep 1; done
+MODDIR=${0%/*}
+. "$MODDIR/config"
 
-	MODDIR=${0%/*}
-	. "$MODDIR/config"
+rm -f "/data/adb/rvelo/${MODDIR##*/}.apk"
+rmdir "/data/adb/rvelo"
 
-	rm "/data/adb/rvelo/${MODDIR##*/}.apk"
-	rmdir "/data/adb/rvelo"
-	rm "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
-} &
+rm -f "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"